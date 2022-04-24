77c286066e3b2c3ce6f266d3e2f33e90bbf6e655
VK <vitaliy.krynytskyy@gmail.com>
2022-04-24 14:35:28 +0100

    Updated the App - essentially completed the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+
+const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+const App = () => {
+    const [greeting, setGreetingValue] = useState('');
+
+    const fetchGreeting = async () => {
+        if (typeof window.ethereum !== 'undefined') {
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+
+            try {
+                const data = await contract.greet();
+
+                setGreetingValue(data);
+
+                console.log(`data: ${data}`);
+            } catch (err) {
+                console.log(`Error: ${err}`);
+            }
+        }
+    }
+
+    const setGreeting = async value => {
+        if (!value) return;
+
+        if (!typeof window.ethereum !== 'undefined') {
+            await requestAccount();
+
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+            const transaction = await contract.setGreeting(value);
+
+            await transaction.wait();
+
+            fetchGreeting();
+        }
+    }
+
+    const requestAccount = async () => await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+    const handleSubmit = async event => {
+        event.preventDefault();
+        await setGreeting(event.target.greetingInput.value);
+        event.target.greetingInput.value = '';
+    }
+
+    return (
+        <div className='w-full max-w-lg container'>
+            <div className='shadow-md rounded px-8 pt-6 pb-8 mb-4 mt-4'>
+                <div className='text-gray-600 font-bold text-lg mb-2'>
+                    React Ethereum Dapp
+                </div>
+                <div className='w-full border-4 p-2 mb-4 rounded border-gray-400'>
+                    <div className='text-gray-600 font-bold text-md mb-2'>
+                        Fetch Greeting Message From Smart Contract
+                    </div>
+                    <div className='flex'>
+                        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded' onClick={fetchGreeting}>Fetch Greeting</button>
+                    </div>
+                </div>
+                <div className='w-full border-4 p-2 mb-4 rounded border-gray-400'>
+                    <div className='text-gray-600 font-bold text-md mb-2'>
+                        Set Greeting Message On Smart Contract
+                    </div>
+                    <form
+                        className='flex items-center justify-between'
+                        onSubmit={event => handleSubmit(event)}
+                    >
+                        <input
+                            className='shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                            name='greetingInput'
+                        />
+                        <button className='bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Set Greeting</button>
+                    </form>
+                </div>
+                <div className='w-full border-4 p-2 mb-4 rounded border-gray-400 bg-gray-100'>
+                    <div className='text-gray-600 font-bold text-md mb-2'>
+                        Greeting Message
+                    </div>
+                    <p>
+                        {greeting}
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
